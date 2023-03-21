701a09b43b601eee6573bde033fcf38eefbadd2e
Shlomi Nugarker <shlomin1231@gmail.com>
2023-03-21 12:46:53 +0200

    orgnize file

--- src/ts/controller/onSetNumberInCell.ts
+++ src/ts/controller/onSetNumberInCell.ts
@@ -1,20 +1,7 @@
 import { setNumberInMat } from '../services/game'
+import onUpdateCell from './onUpdateCell'
 
 export default function onSetNumberInCell(num: string) {
   const updatedCell = setNumberInMat(num)
   onUpdateCell(updatedCell)
-}
-
-const onUpdateCell = (
-  updatedCell: {
-    num: string
-    i: number
-    j: number
-  } | null
-) => {
-  if (!updatedCell) return
-  const elToUpadate = document.getElementById(
-    `${updatedCell.i}-${updatedCell.j}`
-  )
-  if (elToUpadate) elToUpadate.innerHTML = updatedCell.num
 }
--- src/ts/services/utils.ts
+++ src/ts/services/utils.ts
@@ -1,10 +1,10 @@
 export { shuffleArray, getRandomInt }
 
 function shuffleArray<T>(array: T[]) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1))
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
 
-    var temp = array[i]
+    const temp = array[i]
     array[i] = array[j]
     array[j] = temp
   }
