4a16db70c3155bdef52aff17fdb04b01e6ed0197
Shlomi Nugarker <shlomin1231@gmail.com>
2023-03-28 17:10:57 +0300

    getUniqueObjects func

--- src/ts/services/utils.ts
+++ src/ts/services/utils.ts
@@ -1,4 +1,4 @@
-export { shuffleArray, getRandomInt }
+export { shuffleArray, getRandomInt, getUniqueObjects }
 
 function shuffleArray<T>(array: T[]) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -16,4 +16,15 @@ function getRandomInt(min: number, max: number) {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function getUniqueObjects<T>(array: T[]): T[] {
+  const uniqueObjects: { [key: string]: any } = {}
+
+  array.forEach((obj) => {
+    const str = JSON.stringify(obj)
+    uniqueObjects[str] = obj
+  })
+
+  return Object.values(uniqueObjects)
 }
