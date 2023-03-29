e23eb3d595dcbfa6c2b992cc73f09f32183de184
Shlomi Nugarker <shlomin1231@gmail.com>
2023-03-30 00:48:15 +0300

    markAllSimilarNums

--- src/ts/controller/onCellClicked.ts
+++ src/ts/controller/onCellClicked.ts
@@ -1,15 +1,19 @@
 import { setSelectedCell } from '../services/game'
-import cleanAffectedCells from './cleanAffectedCells'
 import markAffectedCells from './markAffectedCells'
+import markAllSimilarNums from './markAllSimilarNums'
 
 export function onCellClicked(cell: Element) {
   document
     .querySelectorAll('.selected')
     .forEach((cell) => cell.classList.remove('selected'))
-
   setSelectedCell(cell)
   cell.classList.add('selected')
 
-  cleanAffectedCells()
+  // cleanAffectedCells
+  document
+    .querySelectorAll('.affected')
+    .forEach((cell) => cell.classList.remove('affected'))
   markAffectedCells(cell)
+
+  markAllSimilarNums(cell)
 }
