3cc6b1d962b86fa382a1d6734224532283131cfc
Shlomi Nugarker <shlomin1231@gmail.com>
2023-03-30 02:09:25 +0300

    addListeners - refactor

--- src/ts/controller/onLoad.ts
+++ src/ts/controller/onLoad.ts
@@ -1,23 +1,10 @@
 import { createBoard } from '../services/game'
-import { onCellClicked } from './onCellClicked'
-import onKeyUp from './onKeyUp'
-import onSetNumberInCell from './onSetNumberInCell'
+import addListeners from './addListeners'
+
 import { renderBoard } from './renderBoard'
 
 export const onLoad = () => {
   const board = createBoard()
   board && renderBoard(board, '.board')
-
-  const cells = document.querySelectorAll('.cell')
-  cells.forEach((cell) =>
-    cell.addEventListener('click', () => onCellClicked(cell))
-  )
-
-  document.querySelectorAll('.num-btn').forEach((numBtn) =>
-    numBtn.addEventListener('click', () => {
-      onSetNumberInCell(numBtn.textContent || '')
-    })
-  )
-
-  window.addEventListener('keyup', onKeyUp)
+  addListeners()
 }
