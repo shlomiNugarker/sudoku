106b40f44e86b7721b88cad0af1fc5ca596e0a58
Shlomi Nugarker <shlomin1231@gmail.com>
2023-03-30 00:51:08 +0300

    cancel classList.remove

--- src/ts/controller/markAllInvalidCells.ts
+++ src/ts/controller/markAllInvalidCells.ts
@@ -1,10 +1,6 @@
 import { Coord } from '../../interfaces/coord'
 
 export default function markAllInvalidCells(invalidCoords: Coord[]) {
-  // document
-  //   .querySelectorAll('.red')
-  //   .forEach((cell) => cell.classList.remove('red'))
-
   invalidCoords.forEach((coord) => {
     document.getElementById(`${coord.i}-${coord.j}`)?.classList.add('red')
   })
